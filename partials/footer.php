289fbf6c530b5e58a3f5733dd526827e40bec98e
GrayAdr03 <clemente.adrian897@gmail.com>
2026-05-07 23:33:16 -0400

    Delete partials directory

--- partials/footer.php
+++
@@ -1,7 +0,0 @@
-    <?php require __DIR__ . '/auth_modals.php'; ?>
-    <?php require __DIR__ . '/cart_modal.php'; ?>
-    <script src="assets/js/main.js"></script>
-    <script src="assets/js/checkout.js"></script>
-</body>
-</html>
-
